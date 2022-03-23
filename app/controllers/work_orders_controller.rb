d32463318c6df8255db01a95fd9353ae7e94e311
devomos <dev@grupofemal.com>
2022-03-22 17:40:29 -0700

    Validates work orders

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -1,16 +1,17 @@
 class WorkOrdersController < ApplicationController
   def index
-    @times = ["06:00","07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"]
+    @times = ["06:00","07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00",]
     @work_orders = WorkOrder.order(:time)
     @technicians = Technician.all
+    @dom = nil
+    @available_time = nil
     grid_headers
   end
 
   def grid_headers
-    array = ["Time"]
+    @headers = ["Time"]
     Technician.all.each do |technician|
-      array << technician.name
+      @headers << technician.name
     end
-    @headers = array
   end
 end
--- app/helpers/work_orders_helper.rb
+++ app/helpers/work_orders_helper.rb
@@ -1,5 +1,25 @@
 module WorkOrdersHelper
   def time_valid?(work_time, start_time, end_time)
-    start_time.between?(work_time, work_time + 1.hour - 1.minute) || end_time.between?(work_time, work_time + 1.hour ) || work_time.between?(start_time, end_time)
+    start_time.between?(work_time, work_time + 59.minutes) || end_time.between?(work_time + 1.minute, work_time + 1.hour ) || work_time.between?(start_time, end_time - 1.minute)
+  end
+
+  def validate_orders(technician, work_time)
+    last_time = Time.parse("06:00")
+    @order_info = nil
+    technician.work_orders.order(:time).each do |order|
+       start_time = Time.parse(order.time.strftime("%H:%M"))
+       end_time = start_time + order.duration.minutes
+       if start_time == last_time
+         last_time = end_time
+       end
+       if time_valid?(work_time, start_time, end_time)
+         @order_info = order
+         last_time = end_time
+       elsif last_time <= work_time && last_time < start_time
+         @available_time = start_time - last_time
+         last_time = end_time
+         @dom = order
+       end
+     end
   end
 end
